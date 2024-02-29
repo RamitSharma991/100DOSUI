cee8ef76b0a5eaed2483b77dd9bca39c79a6a006
RAMIT SHARMA <ramitsharma_apple@icloud.com>
2024-02-29 19:28:39 +0530

    first commit

---
+++ FlagQuiz/FlagQuiz/ContentView.swift
@@ -0,0 +1,137 @@
+//
+//  ContentView.swift
+//  FlagQuiz
+//
+//  Created by Ramit sharma on 28/02/24.
+//
+
+import SwiftUI
+
+
+struct ContentView: View {
+    
+    @State private var showingScore = false
+    @State private var scoreTitle = ""
+    @State private var score = 0
+    @State private var gameReset = false
+    @State private var timesTapped = 0
+    
+    @State var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Spain", "UK", "Ukraine", "US"].shuffled()
+    @State var correctAnswer = Int.random(in: 0...2)
+    
+    
+    var body: some View {
+        
+        ZStack {
+            RadialGradient(stops: [.init(color: Color(red: 0.1, green: 0.2, blue: 0.45), location: 0.3), .init(color: Color(red: 0.76, green: 0.15, blue: 0.26), location: 0.3)], center: .top, startRadius: 200, endRadius: 700)
+                .ignoresSafeArea()
+            
+            VStack {
+                Spacer()
+                Text("Guess the Flag")
+                    .font(.largeTitle.bold())
+                    .foregroundStyle(.white)
+                VStack(spacing: 30) {
+                    VStack(spacing: 10) {
+                        Text("Tap the flag of")
+                            .foregroundStyle(.secondary)
+                            .font(.headline.weight(.heavy))
+                        
+                        Text(countries[correctAnswer])
+                            .foregroundStyle(.white)
+                            .font(.largeTitle.weight(.semibold))
+                    }
+                    
+                    ForEach(0..<3) { number in
+                        Button {
+                            flagTapped(number)
+                        } label: {
+                            Image(countries[number])
+                                .clipShape(.capsule)
+                                .shadow(radius: 10)
+                            
+                        }
+                    }
+                }
+                .frame(maxWidth: .infinity)
+                .padding(.vertical, 20)
+                .background(.thinMaterial)
+                .clipShape(.rect(cornerRadius: 20))
+                Spacer()
+                Spacer()
+                
+                VStack(spacing: 5) {
+                    Text("Score: \(score)")
+                    Text("Attempts: \(timesTapped)/8")
+                }
+                .foregroundStyle(.white)
+                .font(.title.bold())
+                .frame(width: 200, height: 100)
+                .background(.ultraThinMaterial)
+                .clipShape(.rect(cornerRadius: 20))
+                
+                
+                
+                
+                Spacer()
+                
+            }
+            .padding()
+        }
+        .alert(scoreTitle, isPresented: $showingScore) {
+            if timesTapped < 8 {
+                Button("Continue", action: askQuestion)
+            } else {
+                Button("Restart", action: reset)
+            }
+        } message: {
+            Text("\(8 - timesTapped) attempts remaining")
+        }
+        
+    }
+    
+    
+    func flagTapped(_ number: Int) {
+        
+        timesTapped += 1
+        if number == correctAnswer {
+            scoreTitle = "Correct Answer! "
+            score += 1
+        }
+        else {
+            scoreTitle = """
+            Wrong! ⚠️
+            That's the flag of \(countries[number])
+            """
+            score -= 1
+        }
+        
+        showingScore = true
+    }
+    
+    func askQuestion() {
+        if timesTapped < 8 {
+            countries.shuffle()
+            correctAnswer = Int.random(in: 0...2)
+        } else {
+            scoreTitle = "Game Resets after 8 attempts"
+            showingScore = true
+            
+        }
+        
+        
+    }
+    
+    func reset() {
+        score = 0
+        timesTapped = 0
+        askQuestion()
+    }
+    
+}
+
+#Preview {
+    ContentView()
+}
+
+
---
+++ FlagQuiz/FlagQuiz/FlagQuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlagQuizApp.swift
+//  FlagQuiz
+//
+//  Created by Ramit sharma on 28/02/24.
+//
+
+import SwiftUI
+
+@main
+struct FlagQuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
