4871f99ef11434d8067445a467b301cc95e80a5b
RAMIT SHARMA <ramitsharma_apple@icloud.com>
2024-03-08 01:00:49 +0530

    Brainy Game Added

---
+++ BrainyGame/BrainyGame/BrainyGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrainyGameApp.swift
+//  BrainyGame
+//
+//  Created by Ramit sharma on 06/03/24.
+//
+
+import SwiftUI
+
+@main
+struct BrainyGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
