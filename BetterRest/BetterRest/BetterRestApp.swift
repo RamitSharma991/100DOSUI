227852fad179e75bce1d941215b3c61f0b164b28
RAMIT SHARMA <ramitsharma_apple@icloud.com>
2024-03-11 13:50:49 +0530

    Added Better Rest Core ML App

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Ramit sharma on 11/03/24.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
