43b9018109b71968029ba68db7cf3c41faba13cb
RAMIT SHARMA <ramitsharma_apple@icloud.com>
2024-03-15 19:44:35 +0530

    Added WordScramble

---
+++ WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Ramit sharma on 15/03/24.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
