3dd11b0b21d08070b3d280b829aafa58466f14c3
RAMIT SHARMA <ramitsharma_apple@icloud.com>
2024-03-20 23:44:35 +0530

    Added IExpense App

---
+++ iExpense/iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Ramit sharma on 20/03/24.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
