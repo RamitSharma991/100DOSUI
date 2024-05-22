e0b580d543cf4a5d9788b0033d48b25bab64146a
Ramit Sharma <ramitsharma991@gmail.com>
2024-05-22 23:02:19 +0530

    Comparable, documents directory, MapKit, Biometric authentication

--- BucketList/BucketList/ContentView.swift
+++ BucketList/BucketList/ContentView.swift
@@ -5,17 +5,67 @@
 //  Created by Ramit Sharma on 17/05/24.
 //
 
+import MapKit
 import SwiftUI
 
 struct ContentView: View {
+    @State private var viewModel = ViewModel()
+    @State private var mapType: MKMapType = .standard
+    
+    
+    let startPosition = MapCameraPosition.region(
+        MKCoordinateRegion(
+            center: CLLocationCoordinate2D(latitude: 56, longitude: -3),
+            span: MKCoordinateSpan(latitudeDelta: 10, longitudeDelta: 10)
+        )
+    )
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        
+            if viewModel.isUnlocked {
+                VStack {
+                    Picker("Map Type", selection: $mapType) {
+                        Text("Standard").tag(MKMapType.standard)
+                        Text("Hybrid").tag(MKMapType.hybrid)
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
+                    .padding(10)
+                    
+                }
+                MapReader { proxy in
+                    Map(initialPosition: startPosition) {
+                        ForEach(viewModel.locations) { location in
+                            Annotation(location.name, coordinate: location.coordinate) {
+                                Image(systemName: "star.circle")
+                                    .resizable()
+                                    .foregroundStyle(.red)
+                                    .frame(width: 44, height: 44)
+                                    .background(.white)
+                                    .clipShape(.circle)
+                                    .onLongPressGesture {
+                                        viewModel.selectedPlace = location
+                                }
+                            }
+                        }
+                    }
+                    .onTapGesture { position in
+                        if let coordinate = proxy.convert(position, from: .local) {
+                            viewModel.addLocation(at: coordinate)
+                        }
+                    }
+                    .sheet(item: $viewModel.selectedPlace) { place in
+                        EditView(location: place) {
+                            viewModel.update(location: $0)
+                        }
+                    }
+                }
+            } else {
+                Button("Unlock Places", action: viewModel.authenticate)
+                    .padding()
+                    .background(.blue)
+                    .foregroundStyle(.white)
+                    .clipShape(.capsule)
         }
-        .padding()
     }
 }
 
